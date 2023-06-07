166408a5d68a13459495e0b1fcd70fa82f29a038
j14so <j14solano23@gmail.com>
2023-06-07 02:33:15 -0300

    Merge origin/BryanBranch into BryanBranch
    Conflicts:
    	src/main/java/org/example/Menu.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,19 +2,19 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-//        Menu menu = new Menu();
-//        menu.startMenu();
+        Menu menu = new Menu();
+        menu.startMenu();
 
-        BinarySearchTrees binarySearchTrees = new BinarySearchTrees();
-        binarySearchTrees.insertar(5, null);
-        binarySearchTrees.insertar(2, null);
-        binarySearchTrees.insertar(1, null);
-        binarySearchTrees.insertar(3, null);
-        binarySearchTrees.insertar(8, null);
-        binarySearchTrees.insertar(6, null);
-        binarySearchTrees.insertar(10, null);
-
-        binarySearchTrees.inorderTreewalk(binarySearchTrees.root);
+//        BinarySearchTrees binarySearchTrees = new BinarySearchTrees();
+//        binarySearchTrees.insertar(5, null);
+//        binarySearchTrees.insertar(2, null);
+//        binarySearchTrees.insertar(1, null);
+//        binarySearchTrees.insertar(3, null);
+//        binarySearchTrees.insertar(8, null);
+//        binarySearchTrees.insertar(6, null);
+//        binarySearchTrees.insertar(10, null);
+//
+//        binarySearchTrees.inorderTreewalk(binarySearchTrees.root);
 
     }
 }
